dd53ef3c04ed9611d3ea07aa0fed11d0019eeb97
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-07-12 09:57:23 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,8 @@
 #!/usr/bin/puppet
 
-# Install a specific version of flask (2.1.0)
+# Install a specific version of Flask (2.1.0)
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'], # Ensure pip3 is installed first
 }
