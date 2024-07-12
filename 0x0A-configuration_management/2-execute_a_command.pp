3927f0461abd65c9971550b9a23b01afa260522c
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-07-12 09:43:36 +0300

    Update 2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -5,4 +5,14 @@ exec { 'killmenow':
   path        => '/usr/bin',
   onlyif      => '/usr/bin/pgrep -f "killmenow"',
   refreshonly => true,
+  notify      => Exec['check_process_stopped'],
+}
+
+exec { 'check_process_stopped':
+  command     => '/usr/bin/pgrep -f "killmenow"',
+  path        => '/usr/bin',
+  onlyif      => '/usr/bin/pgrep -f "killmenow"',
+  refreshonly => true,
+  provider    => shell,
+  unless      => '/usr/bin/pgrep -f "killmenow"',
 }
