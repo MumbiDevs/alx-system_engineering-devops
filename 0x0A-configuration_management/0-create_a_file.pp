46806b0a667759e653e06b7a059590e62e439d94
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-07-18 12:29:18 +0300

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,18 @@
-# creates a file in /tmp
+# Install necessary packages
+package { 'python3-pip':
+  ensure => installed,
+}
 
+# Install Flask
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin', '/usr/local/bin'],
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+}
+
+# Create the file /tmp/school with the specified content, mode, owner, and group
 file { '/tmp/school':
-  content =>'I love Puppet',
+  content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
