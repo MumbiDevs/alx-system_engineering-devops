52fc753b107c1f3983c6eb07ba1e327335d92bcf
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-07-22 23:13:21 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,20 @@
 # install flask from pip3
-package { 'flask' :
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    }
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'python3-venv':
+  ensure => installed,
+}
+
+exec { 'create_flask_env':
+  command => '/usr/bin/python3 -m venv /opt/flask_env',
+  creates => '/opt/flask_env/bin/activate',
+}
+
+exec { 'install_flask':
+  command     => '/opt/flask_env/bin/pip install Flask==2.1.0',
+  environment => ['PATH=/opt/flask_env/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+  require     => Exec['create_flask_env'],
+}
+
