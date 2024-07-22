d2609bbf0566085f78d37836e8c06e3362bd9ac0
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-07-22 23:21:40 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,19 +1,10 @@
 # install flask from pip3
-package { 'python3-pip':
-  ensure => installed,
-}
-
 exec { 'install_flask':
   command     => '/usr/bin/pip3 install Flask==2.1.0',
   environment => ['PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
   require     => Package['python3-pip'],
 }
 
-exec { 'install_werkzeug':
-  command     => '/usr/bin/pip3 install Werkzeug==2.0.0',
-  environment => ['PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
-  require     => Exec['install_flask'],
-}
 
 
 
