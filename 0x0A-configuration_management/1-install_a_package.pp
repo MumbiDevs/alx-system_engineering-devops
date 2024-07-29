733d5d5f1e8b226cbc9f86f5106171da5ea2cf46
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-07-30 00:11:37 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,24 @@
-# install flask from pip3
-package { 'flask':
+# Ensure Python 3.8 is installed using the system package manager
+package { 'python3.8':
+  ensure => installed,
+}
+
+# Ensure pip is installed using the system package manager
+package { 'python3-pip':
+  ensure  => installed,
+  require => Package['python3.8'],
+}
+
+# Install Flask using pip3
+package { 'Flask':
   ensure    => '2.1.0',
   provider  => 'pip3',
+  require   => Package['python3-pip'],
 }
-package { 'python3.8':
-  ensure   => '3.8.10,
-  provider => 'pip3',
-}
+
+# Install Werkzeug using pip3 and ensure it requires Flask
 package { 'Werkzeug':
   ensure    => '2.1.1',
   provider  => 'pip3',
-  require  => Package['Flask'],
+  require   => Package['Flask'],
 }
-
